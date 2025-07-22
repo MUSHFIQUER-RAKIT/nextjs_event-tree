79b11b82565f414fe820a94c886a5938e7793e97
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2025-07-22 15:05:16 +0600

    Implement UI

---
+++ components/landing/Search.jsx
@@ -0,0 +1,11 @@
+export default function Search() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search an Event"
+        className="bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md"
+      />
+    </div>
+  );
+}
